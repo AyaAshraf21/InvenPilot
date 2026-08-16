3fac5d7e714a8171f00aceaa00fcd6a1a32b12f2
AyaAshraf21 <aya851821@gmail.com>
2026-08-16 22:06:07 +0300

    Add unit tests for DeleteCategoryHandler covering missing category, and successful return

---
+++ InvenPilot.Tests/Categories/DeleteCategoryHandlerTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using InvenPilot.Application.Exceptions;
+using InvenPilot.Application.Features.Categories.Commands.DeleteCategory;
+using InvenPilot.Application.Features.Categories.Commands.UpdateCategory;
+using InvenPilot.Application.Features.Categories.DTO;
+using InvenPilot.Application.Interfaces;
+using InvenPilot.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvenPilot.Tests.Categories
+{
+    public class DeleteCategoryHandlerTests
+    {
+        [Fact]
+        public async Task Handle_WhenCategoryIDNotFound_ShouldThrowNotFoundException()
+        {
+            var categoryRepositoryMock = new Mock<ICategoryRepository>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var category = new Category
+            {
+                ID = 1,
+                Name = "TVs"
+            };
+
+            var request = new DeleteCategoryCommand(1);
+
+            categoryRepositoryMock.Setup(x => x.GetCategoryByIdAsync(1)).ReturnsAsync((Category?)null);
+
+            var handler = new DeleteCategoryHandler(categoryRepositoryMock.Object, unitOfWorkMock.Object);
+
+            await Assert.ThrowsAsync<NotFoundException>(() => handler.Handle(request, CancellationToken.None));
+
+            categoryRepositoryMock.Verify(
+                x => x.DeleteCategory(It.IsAny<Category>()),
+                Times.Never
+            );
+
+            unitOfWorkMock.Verify(
+                x => x.SaveChangesAsync(It.IsAny<CancellationToken>()),
+                Times.Never
+            );
+        }
+
+        [Fact]
+        public async Task Handle_WhenRequestValid_ShouldDeleteCategory()
+        {
+            var categoryRepositoryMock = new Mock<ICategoryRepository>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var category = new Category
+            {
+                ID = 1,
+                Name = "TVs"
+            };
+
+            var request = new DeleteCategoryCommand(1);
+
+            categoryRepositoryMock.Setup(x => x.GetCategoryByIdAsync(1)).ReturnsAsync(category);
+
+            var handler = new DeleteCategoryHandler(categoryRepositoryMock.Object, unitOfWorkMock.Object);
+
+            var result = handler.Handle(request, CancellationToken.None);
+
+            categoryRepositoryMock.Verify(
+                x => x.DeleteCategory(It.IsAny<Category>()),
+                Times.Once
+            );
+
+            unitOfWorkMock.Verify(
+                x => x.SaveChangesAsync(It.IsAny<CancellationToken>()),
+                Times.Once
+            );
+        }
+    }
+}
